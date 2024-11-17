2611087ac6869c9b4874ab3f73690bb5022f8cb8
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-11-17 21:52:17 +0000

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,3 +8,4 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 ["class CustomUser(AbstractUser):", "date_of_birth", "profile_photo"]
+["class CustomUserManager(BaseUserManager):", "create_user", "create_superuser"]
