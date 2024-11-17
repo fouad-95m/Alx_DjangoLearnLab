3bfae9c91883ec477c67996147e534aee0799907
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-11-17 21:52:41 +0000

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -12,3 +12,4 @@ class BookAdmin(admin.ModelAdmin):
     
     # Add filter options for publication year
     list_filter = ('publication_year',)
+    ["admin.site.register(CustomUser, CustomUserAdmin)"]
