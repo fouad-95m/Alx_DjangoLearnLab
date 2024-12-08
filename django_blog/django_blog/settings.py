5d69adea78a01fa15cb5b5f1b6ddb4638de9d766
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-12-08 19:46:44 +0000

    Add files via upload

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -115,16 +115,23 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_DIRS = [
-BASE_DIR / "static",
-]
 
 TEMPLATES = [
     {
-    'DIRS': [BASE_DIR / 'templates'],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 # Default primary key field type
